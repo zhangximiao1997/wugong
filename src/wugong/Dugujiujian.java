62eb54298a8ab86bc4b6b8c6ef85addd1c6898be
zhoudie <diedie0228@qq.com>
2019-12-05 16:01:59 +0800

    444

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -5,6 +5,8 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("11111");
 		System.out.println("22222");
+		System.out.println("33333");
+		System.out.println("44444");
 	}
 
 }
